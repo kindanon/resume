d8097f784e025c25aeaaad3db5d78be658ecb4c9
Ethan Manarin <35640579+kindanon@users.noreply.github.com>
2019-09-08 20:00:51 -0400

    added actual stuff
    inital setup for the nth time

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//smooth scroll for webpage
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  });
