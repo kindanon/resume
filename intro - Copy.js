8384e26e6f3d1c74f064d8c4cf6a04fab1527c9a
Ethan Manarin <35640579+kindanon@users.noreply.github.com>
2019-10-07 19:44:37 -0400

    attempting side animations

--- intro - Copy.js
+++ intro - Copy.js
@@ -1,38 +1,37 @@
-var right = document.querySelector('.right');
-var left = document.querySelector('.left');
 var timeControlEl = document.querySelector('.parent');
 var timeCursorEl = document.querySelector('.profile-pic');
 var width = window.innerWidth;
 var windowHeight = window.innerHeight;
 var scrollAnim;
 
+var animationPXOffset = (timeControlEl.offsetWidth - (timeControlEl.parentNode.offsetWidth - 20)) / 2;
+if (animationPXOffset < 0) animationPXOffset = 0;
+
 var timelineAnimation = anime.timeline({
     easing: 'linear',
     autoplay: false
 })
     .add({
+        targets: timeControlEl,
+        translateX: [animationPXOffset, -animationPXOffset],
+        duration: 1500
+    }, 0)
+    .add({
         targets: timeCursorEl,
+        translateZ: 0,
         keyframes: [
-            { translateX: width-200, duration: 3500},
+           // { translateY: [-24, 0], duration: 100, easing: 'easeInQuad' },
+            { translateX: width-200, duration: 3500 },
+           // { translateY: -24, duration: 100, easing: 'easeOutQuad' }
         ],
         duration: 1500
     }, -100)
 
-    .add({
-        targets: right,
-        keyframes: [
-            { translateX: width-200, duration: 3500},
-        ],
-        duration: 1500
-    }, -100)
-
-    .add({
-        targets: left,
-        keyframes: [
-            { translateX: width-200, duration: 3500},
-        ],
-        duration: 1500
-    }, -100)
+var time = {
+    anim: null,
+    start: 0,
+    end: 0
+};
 
 function moveControlAnimation() {
     var rect = timeControlEl.getBoundingClientRect();
@@ -43,10 +42,18 @@ function moveControlAnimation() {
     if (true) scrollAnim = requestAnimationFrame(moveControlAnimation);
 }
 
-isElementInViewport(timeCursorEl, function (el, entry) {
+isElementInViewport(timeControlEl, function (el, entry) {
     windowHeight = window.innerHeight;
     controlAnimationCanMove = true;
     moveControlAnimation();
 }, function (el, entry) {
     controlAnimationCanMove = false;
 }, '50px');
+
+onScroll(function () {
+    if (time.anim && !time.anim.paused) {
+        time.anim.pause();
+        controlAnimationCanMove = true;
+        moveControlAnimation();
+    }
+});
