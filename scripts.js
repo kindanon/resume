49460b33d3b4558b17453bd5bffbf7d4bdfcf675
Ethan Manarin <35640579+kindanon@users.noreply.github.com>
2019-10-31 18:40:52 -0400

    new content formatting

--- scripts.js
+++ scripts.js
@@ -5,9 +5,7 @@ function loading(page){
         url: page,
         data: { },
         success: function(data){
-            $("#loading_zone").animate({height: "0rem"});
             $('#loading_zone').html(data);
-            $("#loading_zone").animate({height: "25rem"});
 
         },error: function(){
             console.log("ajax failed");
