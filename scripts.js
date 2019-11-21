b24846cd25bdfdede47465743b606f5f960dd3ee
Ethan Manarin <35640579+kindanon@users.noreply.github.com>
2019-11-21 14:21:53 -0500

    added modal stuff

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
-
 //homepage planet orbits
 var system1;
 var system2;
 var system3;
 
 function orbits(orbit, planet, dura){
-    var path = anime.path('.solarSystem .' + orbit);
+    var path = anime.path('.solarSystem #' + orbit);
     var orbits = anime({
-      targets: '.solarSystem .' + planet,
+      targets: '.solarSystem #' + planet,
       translateX: path('x'),
       translateY: path('y'),
       rotate: path('angle'),
@@ -23,16 +22,34 @@ function orbits(orbit, planet, dura){
     system3 = orbits('orbit3', 'planet3', 60000);
     }
     
-    document.querySelector('.planet1').onmouseover  = function(){system1.pause();}
-    document.querySelector('.planet1').onmouseleave = function(){system1.play();}
+    document.querySelector('#planet1').onmouseover  = function(){system1.pause();}
+    document.querySelector('#planet1').onmouseleave = function(){system1.play();}
+
+    document.querySelector('#planet2').onmouseover  = function(){system2.pause();}
+    document.querySelector('#planet2').onmouseleave = function(){system2.play();}
     
-    document.querySelector('.planet2').onmouseover  = function(){system2.pause();}
-    document.querySelector('.planet2').onmouseleave = function(){system2.play();}
-    
-    document.querySelector('.planet3').onmouseover  = function(){system3.pause();}
-    document.querySelector('.planet3').onmouseleave = function(){system3.play();}
+    document.querySelector('#planet3').onmouseover  = function(){system3.pause();}
+    document.querySelector('#planet3').onmouseleave = function(){system3.play();}
     
 //homepage stars
 particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+//homepage modals
+var modal1 = document.querySelector('#planet1m');
+var modal2 = document.querySelector('#planet2m');
+var modal3 = document.querySelector('#planet3m');
+
+var planet1 = document.querySelector('#planet1m');
+var planet2 = document.querySelector('#planet2m');
+var planet3 = document.querySelector('#planet3m');
+
+planet1.onclick = function(){modal1.style.display = "block";}
+window.onclick = function(event) {if (event.target == modal1) {modal1.style.display = "none";}}
+
+planet2.onclick = function(){modal2.style.display = "block";}
+window.onclick = function(event) {if (event.target == modal2) {modal2.style.display = "none";}}
+
+planet3.onclick = function(){modal3.style.display = "block";}
+window.onclick = function(event) {if (event.target == modal3) {modal3.style.display = "none";}}
