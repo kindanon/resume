f663b790a08138973c720a5ac31fa8c9c07f3ed6
Ethan Manarin <35640579+kindanon@users.noreply.github.com>
2019-12-09 12:50:16 -0500

    removed ie testing
    turns out IE doesnt support supports, so i give up
    fixed some scaling issues on mobile

--- scripts.js
+++ scripts.js
@@ -16,24 +16,25 @@ function orbits(orbit, planet, dura){
       loop: true
     });
     return orbits;
-    }
-    window.onload = function() {
-    system1 = orbits('orbit1', 'planet1', 25000);
-    system2 = orbits('orbit2', 'planet2', 90000);
-    system3 = orbits('orbit3', 'planet3', 60000);
-    system0 = orbits('orbit0', 'star', 1000);
-    }
+}
+window.onload = function() {
+  system1 = orbits('orbit1', 'planet1', 25000);
+  system2 = orbits('orbit2', 'planet2', 90000);
+  system3 = orbits('orbit3', 'planet3', 60000);
+  system0 = orbits('orbit0', 'star', 1000);
+}
     
-    document.querySelector('#planet1').onmouseover  = function(){system1.pause();}
-    document.querySelector('#planet1').onmouseleave = function(){system1.play();}
+document.querySelector('#planet1').onmouseover  = function(){system1.pause();}
+document.querySelector('#planet1').onmouseleave = function(){system1.play();}
 
-    document.querySelector('#planet2').onmouseover  = function(){system2.pause();}
-    document.querySelector('#planet2').onmouseleave = function(){system2.play();}
+document.querySelector('#planet2').onmouseover  = function(){system2.pause();}
+document.querySelector('#planet2').onmouseleave = function(){system2.play();}
     
-    document.querySelector('#planet3').onmouseover  = function(){system3.pause();}
-    document.querySelector('#planet3').onmouseleave = function(){system3.play();}
+document.querySelector('#planet3').onmouseover  = function(){system3.pause();}
+document.querySelector('#planet3').onmouseleave = function(){system3.play();}
     
 //homepage stars
 particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
-  });
+});
+
